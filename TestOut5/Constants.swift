b3f31e57d8600353a2f2651e3d3e054e829bacde
Brendan Sick <164306585+bsick223@users.noreply.github.com>
2024-09-04 15:43:52 -0700

    Update Constants.swift

--- TestOut5/Constants.swift
+++ TestOut5/Constants.swift
@@ -8,5 +8,5 @@
 import Foundation
 
 struct Constants {
-    static let openAIApiKey = "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"
+    static let openAIApiKey = 
 }
