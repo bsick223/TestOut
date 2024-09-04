a39878b6f7223f1b29e86ea620917beb62575ee2
Brendan Sick <brendansick@gmail.com>
2024-09-04 07:53:19 -0700

    Initial Commit

---
+++ TestOut5/TestOut5App.swift
@@ -0,0 +1,17 @@
+//
+//  TestOut5App.swift
+//  TestOut5
+//
+//  Created by Brendan Sick on 9/4/24.
+//
+
+import SwiftUI
+
+@main
+struct TestOut5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
